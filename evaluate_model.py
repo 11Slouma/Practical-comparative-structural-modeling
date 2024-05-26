9ed8a5d115122bb6c83ca20a3f0571bfd4773ea8
Salim Rebhi <159724868+11Slouma@users.noreply.github.com>
2024-05-25 21:39:23 -1200

    Update evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -7,9 +7,9 @@ env.libs.topology.read(file='$(LIB)/top_heav.lib') # read topology
 env.libs.parameters.read(file='$(LIB)/par.lib') # read parameters
 
 # read model file
-mdl = complete_pdb(env, 'P29957.B99990002.pdb')
+mdl = complete_pdb(env, 'P29957.B99990004.pdb')
 
 # Assess with DOPE:
 s = Selection(mdl)   # all atom selection
-s.assess_dope(output='ENERGY_PROFILE NO_REPORT', file='P29975.profile',
+s.assess_dope(output='ENERGY_PROFILE NO_REPORT', file='MYtemplate1.profile',
               normalize_profile=True, smoothing_window=15)
